c5976f1277e93dada96c254e463e0e25ebd11eaf
dansbands <dansbands@hotmail.com>
2019-02-01 15:35:35 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            I'm a blank Create React App
+          </p>
+          <p>
+            Get Started
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
